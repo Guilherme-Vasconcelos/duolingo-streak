06842a3b88f996969a1da67e5e9307b0d51ffe65
guilherme <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2021-07-22 23:58:44 -0300

    Improve readme

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
 # duolingo-streak
-duolingo-streak is an Emacs package to remind you about your Duolingo daily tasks.
+duolingo-streak is an Emacs package to remind you about your Duolingo daily tasks via D-Bus notifications.
+
 Be aware that duolingo-streak is NOT official (in the sense that it is not endorsed/associated with Duolingo).
 
+### Usage
+Set the environment variables: `DUOLINGO_USERNAME` and `DUOLINGO_PASSWORD`.
+
+To install the package, move `duolingo-streak.el` to some known path and add to your config:
+``` emacs-lisp
+(add-to-list 'load-path "path/to/duolingo-streak.el")  ; Only required if path is not yet added
+(require 'duolingo-streak)
+```
+
+You also need to install the [request](https://github.com/tkf/emacs-request) library (already installed on Doom Emacs).
+
+Then run `(duolingo-streak--verify)` to perform a verification.
+
+You can schedule the function to run hourly by adding to your config:
+``` emacs-lisp
+(run-with-timer 1800 3600 #'duolingo-streak--verify)  ; 1800 is the delay before running for the first time
+```
+
 ### Contact
-If you need to talk to me, please open a new discussion in GitHub Discussions.
+If you'd like to talk to me, please use either the [discussions](https://github.com/Guilherme-Vasconcelos/duolingo-streak/discussions)
+or [issues](https://github.com/Guilherme-Vasconcelos/duolingo-streak/issues) pages.
+
+### Acknowledgements
+- Thanks to the [unofficial Duolingo API wrapper](https://github.com/KartikTalwar/Duolingo) made by KartikTalwar (and contributors), which I have used to check API endpoints.
 
 ### License
 Copyright (C) Guilherme-Vasconcelos
 
-duolingo-streak is licensed under the GNU General Public License, either version 3
-or any later versions. Please refer to LICENSE for more details.
+duolingo-streak is licensed under the GNU General Public License, either version 3 or any later versions. Please refer to LICENSE for more details.
--- duolingo-streak.el
+++ duolingo-streak.el
@@ -1,12 +1,12 @@
 ;;; duolingo-streak.el --- Description -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2021 guilherme
+;; Copyright (C) 2021 Guilherme-Vasconcelos
 ;;
-;; Author:           guilherme <https://github.com/Guilherme-Vasconcelos>
-;; Maintainer:       guilherme <https://github.com/Guilherme-Vasconcelos>
+;; Author:           Guilherme-Vasconcelos <https://github.com/Guilherme-Vasconcelos>
+;; Maintainer:       Guilherme-Vasconcelos <https://github.com/Guilherme-Vasconcelos>
 ;; Version:          0.0.1
-;; Keywords:         duolingo
-;; Homepage:         https://github.com/guilherme/duolingo-streak
+;; Keywords:         duolingo-streak
+;; Homepage:         https://github.com/Guilherme-Vasconcelos/duolingo-streak
 ;; Package-Requires: ((emacs "24.3") (request "0.3.3"))
 
 ;; This file is not part of GNU Emacs.
@@ -32,24 +32,17 @@
 ;;; Commentary:
 ;;
 ;;  This package allows you to verify whether you have done your personal
-;;  daily Duolingo task or not with the function `duolingo-verify'. I
-;;  recommend that you schedule it to run periodically, e.g.:
-;;  (run-with-timer 0 (* 30 60) 'duolingo-verify)
+;;  daily Duolingo task or not with the function `duolingo-streak--verify'.
 ;;
-;;  This works best on Doom Emacs because of icons, env vars, etc.
-;;
-;;  An owl icon will appear at the center of your bottom bar if you need
-;;  to complete your task.
+;;  I recommend that you schedule it to run on every hour with e.g. `run-with-timer'.
 ;;
 ;;  Inspired by: https://github.com/johnvictorfs/dotfiles/blob/master/polybar/scripts/duolingo_streak.py
-;;  TODO: try to remove some progs
-;;  TODO: when parsing login response, use let to bind status-code
-;;  TODO: when parsing login response, also store jwt in a global variable
-;;  TODO: when parsing login response, also check for non 200
+;;  Acknowledgements: Used the unofficial API wrapper https://github.com/KartikTalwar/Duolingo to check API endpoints.
 
 ;;; Code:
 
 (require 'request)
+(require 'notifications)
 
 (defconst duolingo-streak--user-username (getenv "DUOLINGO_USERNAME"))
 (defconst duolingo-streak--user-password (getenv "DUOLINGO_PASSWORD"))
@@ -66,55 +59,74 @@
     (error "Error: you should set the environment variables `DUOLINGO_USERNAME' and `DUOLINGO_PASSWORD'")))
 
 
-(defun duolingo-streak--parse-user-info-response (response)
-  "Parse RESPONSE to verify whether user has completed their daily task or not."
-  (message "%S" (request-response-data response)))  ; TODO
+(defun duolingo-streak--notify-streak-not-extended-today ()
+  "Send a D-Bus notification to warn the user about the daily task."
+  (notifications-notify
+   :title "duolingo-streak"
+   :body "You have not yet completed your daily Duolingo task!"))
 
 
-(defun duolingo-streak--get-user-info-request ()
-  "Send request to Duolingo to get user's data."
+(defun duolingo-streak--check-streak-from-response-data-and-notify (data)
+  "Parse DATA returned in order to verify whether user has completed their daily task or not, then send a notification if needed."
+  (unless (booleanp (assoc-default 'streak_extended_today data))
+    (duolingo-streak--notify-streak-not-extended-today)))
+
+
+(defun duolingo-streak--perform-user-info-request-and-check-streak ()
+  "Get user's data and later check streak."
   (request duolingo-streak--user-info-url
     :headers `(("Authorization" . ,(concat "Bearer " duolingo-streak--jwt-token)) ("User-Agent" . ,duolingo-streak--default-user-agent))
     :parser 'json-read
     :success (cl-function
-              (lambda (&key response &allow-other-keys)
-                (duolingo-streak--parse-user-info-response response)))
+              (lambda (&key data &allow-other-keys)
+                (duolingo-streak--check-streak-from-response-data-and-notify data)))
     :error (cl-function
             (lambda (&key response &allow-other-keys)
               (error "Unexpected error occurred when reading user data. Response: %S" response)))))
 
 
-(defun duolingo-streak--parse-login-response (response)
-  "Parse login RESPONSE from Duolingo and return either an error or the jwt token."
+(defun duolingo-streak--collect-token-and-request-user-data-to-check-streak (response)
+  "Parse login RESPONSE to collect jwt token and later check streak."
   (let ((login-resp-status-code (request-response-status-code response)))
     (if (eq login-resp-status-code 403)
         (error "Duolingo has returned status code 403 forbidden: either change your User Agent, or try again later")
       (unless (eq login-resp-status-code 200)
         (error "Duolingo has returned unexpected status code %s" login-resp-status-code))))
   (setq-default duolingo-streak--jwt-token (request-response-header response "jwt"))
-  (duolingo-streak--get-user-info-request))
+  (when (eq duolingo-streak--jwt-token nil)
+    (error "Could not get jwt token. Response: %s" response))
+  (duolingo-streak--perform-user-info-request-and-check-streak))
 
 
-(defun duolingo-streak--perform-login-and-parse-user-data ()
-  "Send request to Duolingo to log in."
-  (request duolingo-streak--user-login-url
-    :type "POST"
-    :data `(("login" . ,duolingo-streak--user-username) ("password" . ,duolingo-streak--user-password))
-    :headers `(("User-Agent" . ,duolingo-streak--default-user-agent))
-    :parser 'json-read
-    :success (cl-function
-              (lambda (&key response &allow-other-keys)
-                (duolingo-streak--parse-login-response response)))
-    :error (cl-function
-            (lambda (&key response &allow-other-keys)
-              (error "Unexpected error occurred when retrieving jwt token. Response: %S" response)))))
+(defun duolingo-streak--perform-login-and-check-streak ()
+  "Log in and use jwt token to check if user has completed their streak today.
+If jwt-token is already defined, skip the login request and just verify streak instead."
+  (if (eq duolingo-streak--jwt-token nil)
+      (request duolingo-streak--user-login-url
+        :type "POST"
+        :data `(("login" . ,duolingo-streak--user-username) ("password" . ,duolingo-streak--user-password))
+        :headers `(("User-Agent" . ,duolingo-streak--default-user-agent))
+        :parser 'json-read
+        :success (cl-function
+                  (lambda (&key response &allow-other-keys)
+                    (duolingo-streak--collect-token-and-request-user-data-to-check-streak response)))
+        :error (cl-function
+                (lambda (&key response &allow-other-keys)
+                  (error "Unexpected error occurred when retrieving jwt token. Response: %S" response))))
+    (duolingo-streak--perform-user-info-request-and-check-streak)))
 
 
-(defun duolingo-streak--verify-daily-task ()
-  "Verify if user has completed their daily task."
+(defun duolingo-streak--verify ()
+  "Verify if user has extended their streak today and send a D-Bus notification if not."
+
+  ;; Because web API calls are asynchronous, it is important not to rely on luck. So each function
+  ;; calls the other on the `:success' hook.
+  ;; That is the reason all the functions are chained together instead of being separate units.
+  ;; Unfortunately I could not think of a better implementation at the moment.
+
   (interactive)
   (duolingo-streak--assert-env-vars)
-  (duolingo-streak--perform-login-and-parse-user-data))
+  (duolingo-streak--perform-login-and-check-streak))
 
 
 (provide 'duolingo-streak)
